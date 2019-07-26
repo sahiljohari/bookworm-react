5b346aff176d9f062fd0ad19ed401eea3adeff8c
sahiljohari <darewithsj@gmail.com>
2019-07-26 01:24:15 -0500

    Backdrop component and its reducer added

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,15 +1,24 @@
 import React, { useEffect, useReducer } from "react";
 import { Container, Navbar } from "react-bootstrap";
 import ImageCard from "./ImageCard";
+import Backdrop from "./Backdrop";
 
 const API_KEY = "a24ed6015b7ba9bd2c93c4a73ff8a430";
 
 const initialState = {
   items: [],
-  apiKey: "",
-  imageFilePath: "",
+  imageFilePath: "https://image.tmdb.org/t/p",
+  backdropTitle: "",
+  backdropPath: "",
+  backdropSummary: "",
   pageState: "loading"
 };
+
+function sortDataByRating(data) {
+  return data
+    .filter(result => result.vote_average !== 0)
+    .sort((a, b) => b.vote_average - a.vote_average);
+}
 
 function reducer(state, action) {
   switch (action.type) {
@@ -17,8 +26,15 @@ function reducer(state, action) {
       return {
         ...state,
         items: action.payload,
-        imageFilePath: action.imgPath,
         pageState: "loaded"
+      };
+
+    case "getNowPlaying":
+      return {
+        ...state,
+        backdropPath: action.backdropPath,
+        backdropTitle: action.backdropTitle,
+        backdropSummary: action.backdropSummary
       };
 
     default:
@@ -35,8 +51,18 @@ export default function Gallery() {
       .then(json => {
         dispatch({
           type: "getItems",
-          payload: json.results,
-          imgPath: "https://image.tmdb.org/t/p/original/"
+          payload: sortDataByRating(json.results)
+        });
+      });
+
+    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`)
+      .then(res => res.json())
+      .then(json => {
+        dispatch({
+          type: "getNowPlaying",
+          backdropPath: json.results[0].backdrop_path,
+          backdropTitle: json.results[0].title,
+          backdropSummary: json.results[0].overview
         });
       });
   }, []);
@@ -44,22 +70,32 @@ export default function Gallery() {
   let content;
   switch (state.pageState) {
     case "loading":
-      content = <div>Loading...</div>;
+      content = <h1 className="loader">Summoning the movie universe</h1>;
       break;
 
     case "loaded":
       content = (
         <Container>
-          <Navbar bg="dark" variant="dark" sticky="top">
-            <Navbar.Brand href="#home">{" Latest Movies "}</Navbar.Brand>
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home" className="brand">
+              {" Movie Hub "}
+            </Navbar.Brand>
           </Navbar>
+          <Backdrop
+            backdrop_path={
+              state.imageFilePath + `/original/` + state.backdropPath
+            }
+            title={state.backdropTitle}
+            description={state.backdropSummary}
+          />
+
           <br />
           <div className="container-home">
             {state.items.map(item => (
               <ImageCard
                 key={item.id}
                 rating={item.vote_average}
-                title={item.original_title}
+                title={item.title}
                 explanation={item.overview}
                 filename={item.poster_path}
                 filepath={state.imageFilePath}
@@ -73,5 +109,9 @@ export default function Gallery() {
       content = null;
       break;
   }
-  return <div>{content}</div>;
+  return state.pageState === "loaded" ? (
+    <div>{content}</div>
+  ) : (
+    <div className="load-container">{content}</div>
+  );
 }
