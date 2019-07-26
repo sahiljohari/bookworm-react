aad78dc700ef0742f6362710bcd8b9a08b80bd53
sahiljohari <darewithsj@gmail.com>
2019-07-26 01:24:36 -0500

    Removed movie summary

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -1,22 +1,21 @@
 // K0lMq6VQppEbemFDuBE0alvSbDgi1Vk1UU6dpM8h
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 export default function ImageCard(props) {
-  if (props.explanation) {
-    const des = props.explanation;
-    var summary = des.substr(0, des.indexOf(".") + 100) + "...";
-  }
   return (
     <Card bg="dark" text="white">
-      <Card.Header>Rating: {props.rating}/10</Card.Header>
-      <Card.Img variant="top" src={props.filepath + props.filename} />
+      <Card.Header>Rating: {props.rating} / 10</Card.Header>
+      <Card.Img
+        variant="top"
+        src={props.filepath + `/original/` + props.filename}
+      />
       <Card.Body className="c-body">
-        <div>
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Text>{summary}</Card.Text>
-        </div>
-        <Button variant="primary">Read More</Button>
+        {/* <div> */}
+        <Card.Title>{props.title}</Card.Title>
+        {/* <Card.Text>{props.explanation}</Card.Text> */}
+        {/* </div> */}
+        {/* <Button variant="primary">Read More</Button> */}
       </Card.Body>
     </Card>
   );
